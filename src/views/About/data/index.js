94a7439ef20a780e03c76dd0930f0764672537c0
simonhopkins <simon@oldredpanda.com>
2021-06-07 10:47:01 -0400

    edits 2

--- src/views/About/data/index.js
+++ src/views/About/data/index.js
@@ -1,6 +1,6 @@
 export const team = [
   {
-    title: 'Thee Energizer Bunny',
+    title: 'The Energizer Bunny',
     authorPhoto: {  
       src: 'https://mentorsplusangels-data.s3.amazonaws.com/images/GracielaBW.jpg'
     },
@@ -72,7 +72,7 @@ export const team = [
     authorName: 'Eugene Carr',
   },
   {
-    title: 'From Enterprise IT To Latent Developer',
+    title: 'From Enterprise IT To Frustrated Developer',
     authorPhoto: {
       src: 'https://mentorsplusangels-data.s3.amazonaws.com/images/SimonBW.jpg'
     },
